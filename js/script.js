cd0ffed605bc5d3a11e567a6749c3b9fbc887729
AlexFSmirnov <alex.f.smirnov@gmail.com>
2018-11-06 16:56:20 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function setup() {
+    var main    = document.getElementsByClassName("main")[0];
+    var sidebar = document.getElementsByClassName("sidenav")[0];
+
+    // Setting the main margin to that content doesn't overlap with the sidebar 
+    main.style.marginLeft = sidebar.offsetWidth + "px";
+
+    var 
+}
